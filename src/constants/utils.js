84bf99b4b4ea454ec7e99cb6fe519e1a2ef133ef
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 23:17:55 +0900

    Feat: 날짜 formating 함수 추가

---
+++ src/constants/utils.js
@@ -0,0 +1,8 @@
+export const formattedDate = (taskDate) => {
+  const date = new Date(taskDate);
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const today = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const min = String(date.getMinutes()).padStart(2, '0');
+  return `${month}/${today} ${hour}:${min}`;
+};
