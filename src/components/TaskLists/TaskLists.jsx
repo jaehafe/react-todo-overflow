d135ec1198511196f507668660ba103413da5ff1
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 15:44:19 +0900

    Feat: TaskList, TaskLists 컴포넌트 추가

---
+++ src/components/TaskLists/TaskLists.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import TaskList from '../TaskList/TaskList';
+import * as S from './TaskLists.style';
+
+function TaskLists() {
+  return (
+    <>
+      <TaskList />
+    </>
+  );
+}
+
+export default TaskLists;
