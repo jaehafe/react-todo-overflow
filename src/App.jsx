fd30cabeeb7521e173fc1c8cb6c63a42757bf6b5
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 23:21:56 +0900

    Feat: Main.jsx 파일에서 App.jsx로 useState및 주요 함수 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,62 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { GlobalStyles } from './style/GlobalStyle';
 import ResetCss from './style/ResetCss';
 import Header from './components/Header/Header';
 import Main from './components/Main/Main';
+import { axiosInstance } from './constants/axios';
 
 const App = () => {
+  const [taskList, setTaskList] = useState([]);
+  const [taskInputValue, setTaskInputValue] = useState('');
+
+  useEffect(() => {
+    const fetchTask = async () => {
+      try {
+        const res = await axiosInstance.get();
+        console.log(res.data);
+        setTaskList(res.data);
+      } catch (err) {
+        console.log(err);
+        return [];
+      }
+    };
+    fetchTask();
+  }, []);
+
+  const handleTaskSubmit = (e) => {
+    e.preventDefault();
+    let newTask = {
+      title: taskInputValue,
+      done: false,
+    };
+    setTaskList((prev) => [...prev, newTask]);
+    // axios post
+    requestAddTask(taskList);
+  };
+
+  const requestAddTask = async () => {
+    try {
+      await axiosInstance.post({
+        title: taskInputValue,
+        done: false,
+      });
+    } catch (err) {
+      const response = err.response;
+      alert(`${response.status} error: ${response.data.message}`);
+    }
+  };
+
   return (
     <>
       <ResetCss />
       <GlobalStyles />
       <Header />
-      <Main />
+      <Main
+        taskList={taskList}
+        handleTaskSubmit={handleTaskSubmit}
+        taskInputValue={taskInputValue}
+        setTaskInputValue={setTaskInputValue}
+      />
     </>
   );
 };
