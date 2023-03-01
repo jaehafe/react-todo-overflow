2bf3daad851e42755330d91f39cd803291622b33
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 20:08:49 +0900

    Feat: TaskLists.jsx 레이아웃 추가

--- src/components/TaskLists/TaskLists.jsx
+++ src/components/TaskLists/TaskLists.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
-import TaskList from '../TaskList/TaskList';
+import React, { useState } from 'react';
+// import TaskList from '../TaskList/TaskList';
 import * as S from './TaskLists.style';
 
 function TaskLists() {
+  const [completed, setCompleted] = useState('true');
+
   return (
     <>
-      <TaskList />
+      <S.TaskContainer completed={completed}>
+        <S.TaskWrapper>
+          <S.TaskDateWrapper>
+            <S.TaskDate>생성: 2023-03-01</S.TaskDate>
+            <S.TaskDate>수정: 2023-03-02</S.TaskDate>
+          </S.TaskDateWrapper>
+          <S.TaskTitle>123</S.TaskTitle>
+        </S.TaskWrapper>
+        <S.TaskBtnWrapper>
+          <S.TaskBtn>완료</S.TaskBtn>
+          <S.TaskBtn>수정</S.TaskBtn>
+          <S.TaskBtn>완료</S.TaskBtn>
+        </S.TaskBtnWrapper>
+      </S.TaskContainer>
+      {/*  */}
+      <S.TaskContainer completed={completed}>
+        <S.TaskWrapper>
+          <S.TaskDateWrapper>
+            <S.TaskDate>생성: 2023-03-01</S.TaskDate>
+            <S.TaskDate>수정: 2023-03-02</S.TaskDate>
+          </S.TaskDateWrapper>
+          <S.TaskTitle>123</S.TaskTitle>
+        </S.TaskWrapper>
+        <S.TaskBtnWrapper>
+          <S.TaskBtn>완료</S.TaskBtn>
+          <S.TaskBtn>수정</S.TaskBtn>
+          <S.TaskBtn>완료</S.TaskBtn>
+        </S.TaskBtnWrapper>
+      </S.TaskContainer>
     </>
   );
 }
