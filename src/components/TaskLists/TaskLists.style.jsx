79080147afde0facdffc87098abb2c37a8cc6911
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 20:09:16 +0900

    Design: TaskLists.style.jsx 스타일 추가

--- src/components/TaskLists/TaskLists.style.jsx
+++ src/components/TaskLists/TaskLists.style.jsx
@@ -1,1 +1,69 @@
 import styled from 'styled-components';
+
+export const TaskContainer = styled.li`
+  margin-top: 16px;
+  display: flex;
+  // flex-wrap: wrap;
+  justify-content: space-between;
+  align-items: center;
+  padding: 5px;
+  box-shadow: var(--box-shadow);
+  border-radius: 7px;
+
+  background-color: ${(props) =>
+    props.completed === 'true' ? 'rgb(201, 240, 240)' : undefined};
+
+  text-decoration: ${(props) =>
+    props.completed === 'true' ? 'line-through' : undefined};
+
+  color: ${(props) =>
+    props.completed === 'true' ? 'rgba(142, 134, 134, 0.407)' : undefined};
+`;
+
+export const TaskWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 16px;
+  width: 100%;
+`;
+
+export const TaskDateWrapper = styled.div``;
+
+export const TaskDate = styled.span`
+  min-width: 20px;
+`;
+
+export const TaskTitle = styled.span`
+  display: block;
+  // overflow: scroll;
+  padding: 6px;
+  border-radius: 7px;
+
+  &:active,
+  &:focus {
+    outline: none;
+    background-color: rgb(165, 234, 234);
+  }
+`;
+
+export const TaskBtnWrapper = styled.div`
+  display: flex;
+  gap: 6px;
+`;
+
+export const TaskBtn = styled.button`
+  min-width: 60px;
+  height: 36px;
+  padding: 4px;
+  border: 1px solid var(--main-color);
+  border-radius: 7px;
+  box-shadow: var(--box-shadow);
+
+  &:hover {
+    color: #fff;
+    background-color: var(--main-color);
+  }
+  &:active {
+    transform: scale(0.95);
+  }
+`;
