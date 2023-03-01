e271d96c1749ecf569de3029e70aa243a1e0ef07
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-01 23:23:39 +0900

    Feat: [TaskLists.jsx] api로 task 가져온 후 화면에 렌더링

--- src/components/TaskLists/TaskLists.jsx
+++ src/components/TaskLists/TaskLists.jsx
@@ -1,42 +1,34 @@
-import React, { useState } from 'react';
 // import TaskList from '../TaskList/TaskList';
 import * as S from './TaskLists.style';
+import { formattedDate } from '../../constants/utils';
 
-function TaskLists() {
-  const [completed, setCompleted] = useState('true');
-
+function TaskLists({ taskList, handleTaskSubmit }) {
   return (
-    <>
-      <S.TaskContainer completed={completed}>
-        <S.TaskWrapper>
-          <S.TaskDateWrapper>
-            <S.TaskDate>생성: 2023-03-01</S.TaskDate>
-            <S.TaskDate>수정: 2023-03-02</S.TaskDate>
-          </S.TaskDateWrapper>
-          <S.TaskTitle>123</S.TaskTitle>
-        </S.TaskWrapper>
-        <S.TaskBtnWrapper>
-          <S.TaskBtn>완료</S.TaskBtn>
-          <S.TaskBtn>수정</S.TaskBtn>
-          <S.TaskBtn>완료</S.TaskBtn>
-        </S.TaskBtnWrapper>
-      </S.TaskContainer>
-      {/*  */}
-      <S.TaskContainer completed={completed}>
-        <S.TaskWrapper>
-          <S.TaskDateWrapper>
-            <S.TaskDate>생성: 2023-03-01</S.TaskDate>
-            <S.TaskDate>수정: 2023-03-02</S.TaskDate>
-          </S.TaskDateWrapper>
-          <S.TaskTitle>123</S.TaskTitle>
-        </S.TaskWrapper>
-        <S.TaskBtnWrapper>
-          <S.TaskBtn>완료</S.TaskBtn>
-          <S.TaskBtn>수정</S.TaskBtn>
-          <S.TaskBtn>완료</S.TaskBtn>
-        </S.TaskBtnWrapper>
-      </S.TaskContainer>
-    </>
+    <S.TaskListContainer>
+      {taskList.map((task) => {
+        const { id, order, title, done, createdAt, updatedAt } = task;
+        return (
+          <S.TaskContainer
+            done={done ? 'true' : 'false'}
+            key={id}
+            order={order}
+          >
+            <S.TaskWrapper>
+              <S.TaskDateWrapper>
+                <S.TaskDate>생성: {formattedDate(createdAt)}</S.TaskDate>
+                <S.TaskDate>수정: {formattedDate(updatedAt)}</S.TaskDate>
+              </S.TaskDateWrapper>
+              <S.TaskTitle>{title}</S.TaskTitle>
+            </S.TaskWrapper>
+            <S.TaskBtnWrapper>
+              <S.TaskBtn>완료</S.TaskBtn>
+              <S.TaskBtn>수정</S.TaskBtn>
+              <S.TaskBtn>삭제</S.TaskBtn>
+            </S.TaskBtnWrapper>
+          </S.TaskContainer>
+        );
+      })}
+    </S.TaskListContainer>
   );
 }
 
