6b44ea791a3d861d1cff8256124f8bb6bba23fe2
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-03 22:34:36 +0900

    Feat: TaskLists 타입 추가

--- src/components/TaskLists/TaskLists.tsx
+++ src/components/TaskLists/TaskLists.tsx
@@ -1,25 +1,33 @@
 // import TaskList from '../TaskList/TaskList';
 import * as S from './TaskLists.style';
 import TaskList from '../TaskList/TaskList';
+import { Todo } from '../../constants/request';
+
+interface TaskListsProps {
+  taskData: Todo[];
+  setTaskData: React.Dispatch<React.SetStateAction<Todo[]>>;
+  handleDeleteTask: (id: string) => void;
+  handleCompleteTask: (id: string) => void;
+}
 
 function TaskLists({
   taskData,
   setTaskData,
   handleDeleteTask,
   handleCompleteTask,
-}) {
+}: TaskListsProps) {
   return (
     <S.TaskListContainer>
       {taskData?.map(({ id, title, order, done, createdAt, updatedAt }) => {
         return (
           <TaskList
             key={id}
-            id={id}
-            order={order}
-            title={title}
+            id={id!}
+            order={order!}
+            title={title!}
             done={done}
-            createdAt={createdAt}
-            updatedAt={updatedAt}
+            createdAt={createdAt!}
+            updatedAt={updatedAt!}
             handleDeleteTask={handleDeleteTask}
             handleCompleteTask={handleCompleteTask}
             taskData={taskData}
