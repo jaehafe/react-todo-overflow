b8534b5bdbf855e159c8c1b03008a5206e3d399d
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-03 22:32:33 +0900

    Feat: utils.ts 타입 추가

--- src/constants/utils.ts
+++ src/constants/utils.ts
@@ -1,4 +1,4 @@
-export const formattedDate = (taskDate) => {
+export const formattedDate = (taskDate: any) => {
   const date = new Date(taskDate);
   const month = String(date.getMonth() + 1).padStart(2, '0');
   const today = String(date.getDate()).padStart(2, '0');
