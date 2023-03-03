d4c1fea535c4097fd2c78e578292960b39bb088b
Jaeha Lee <wogkdkrm112@gmail.com>
2023-03-03 18:17:48 +0900

    Feat: request.ts interface, type 추가, axios각 메서드 타입 추가

--- src/constants/request.ts
+++ src/constants/request.ts
@@ -1,28 +1,58 @@
 import { axiosInstance } from './axios';
 
+interface RequestAddTask {
+  title: string;
+  order?: number;
+  done?: boolean;
+}
+
+interface Todo {
+  id: string; // 할 일 ID
+  order: number; // 할 일 순서
+  title: string; // 할 일 제목
+  done: boolean; // 할 일 완료 여부
+  createdAt: string; // 할 일 생성일
+  updatedAt: string; // 할 일 수정일
+}
+
+// type UpdateTaskType = Pick<Todo, 'id' | 'title' | 'done' | 'order'>;
+type UpdateTaskType = Omit<Todo, 'createdAt' | 'updatedAt'>;
+
 /** axios add post */
-export const requestAddTask = async (newTask) => {
+export const requestAddTask = async (
+  newTask: RequestAddTask
+): Promise<Todo[]> => {
   try {
     const res = await axiosInstance.post('/', newTask);
     console.log(res);
     return res.data;
   } catch (err) {
     console.log(err);
+    return [];
+    // throw(err)
   }
 };
 
 /** axios delete */
-export const requestDeleteTask = async ({ id }) => {
+export const requestDeleteTask = async ({
+  id,
+}: {
+  id: string;
+}): Promise<void> => {
   try {
     await axiosInstance.delete(`/${id}`);
-    return;
   } catch (err) {
     console.log(err);
   }
 };
 
 /** axios put */
-export const requestUpdateTask = async ({ id, title, done, order }) => {
+export const requestUpdateTask = async ({
+  id,
+  title,
+  done,
+  order,
+}: UpdateTaskType): Promise<Todo[]> => {
   try {
     const res = await axiosInstance.put(`/${id}`, {
       title,
@@ -33,5 +63,6 @@ export const requestUpdateTask = async ({ id, title, done, order }) => {
     return res.data;
   } catch (err) {
     console.log(err);
+    return [];
   }
 };
